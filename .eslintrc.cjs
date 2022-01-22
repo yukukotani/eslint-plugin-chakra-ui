1855fea964803ff7410fcd6308d2bd7fc7c17ee8
Yuku Kotani <yukukotani@gmail.com>
2022-01-22 14:24:41 +0900

    make eslint work

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,23 @@
 "use strict";
 
 module.exports = {
-  root: true,
-  extends: [
-    "eslint:recommended",
-    "plugin:eslint-plugin/recommended",
-    "plugin:node/recommended",
-  ],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:eslint-plugin/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "unused-imports"],
   env: {
     node: true,
+  },
+  rules: {
+    "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      { vars: "all", varsIgnorePattern: "^_", args: "after-used", argsIgnorePattern: "^_" },
+    ],
   },
   overrides: [
     {
