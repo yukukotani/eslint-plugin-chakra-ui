e12482ba89d2386370591c47cea4270eb878cf5b
Yuku Kotani <yukukotani@gmail.com>
2022-01-22 17:32:21 +0900

    update test

--- src/__tests__/attributes-order.test.ts
+++ src/__tests__/attributes-order.test.ts
@@ -18,25 +18,35 @@ test("test", () => {
   tester.run("attributes-order", attributesOrder, {
     valid: [
       {
+        name: "Sorted style props",
         code: `
           import { Box } from "@chakra-ui/react";
           
-          <Box mx="1" ml="2" px="2" color="red">aaa</Box>
+          <Box as="div" key={key} m="1" px="2" py={2} fontSize="md" onClick={onClick} {...props}>Hello</Box>
+        `,
+      },
+      {
+        name: "Not chakra element",
+        code: `
+          import { NotChakra } from "not-chakra";
+          
+          <NotChakra m="1" fontSize="md" px="2" py={2}>Hello</NotChakra>
         `,
       },
     ],
     invalid: [
       {
+        name: "Not sorted",
         code: `
-        import { Box } from "@chakra-ui/react";
-        
-        <Box mx="1" ml="2" color="red" mr="2" px="2">aaa</Box>
+          import { Box } from "@chakra-ui/react";
+            
+          <Box px="2" as="div" onClick={onClick} m="1" key={key} {...props} fontSize="md" py={2}>Hello</Box>
       `,
         errors: [{ messageId: "invalidOrder" }],
         output: `
-        import { Box } from "@chakra-ui/react";
-        
-        <Box mx="1" ml="2" mr="2" px="2" color="red">aaa</Box>
+          import { Box } from "@chakra-ui/react";
+            
+          <Box as="div" key={key} m="1" px="2" py={2} fontSize="md" onClick={onClick} {...props}>Hello</Box>
       `,
       },
     ],
