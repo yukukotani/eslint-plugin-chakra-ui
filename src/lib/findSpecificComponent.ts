67cd3b7945867312df3f1325ad2ee4096a07ce77
Yuku Kotani <yukukotani@gmail.com>
2022-02-12 04:27:05 +0900

    refactor

---
+++ src/lib/findSpecificComponent.ts
@@ -0,0 +1,27 @@
+import { getNonShorthand } from "./getShorthand";
+
+const attributeMap: Record<string, Record<string, string>> = {
+  display: {
+    flex: "Flex",
+    grid: "Grid",
+  },
+  as: {
+    img: "Image",
+  },
+};
+
+export function findSpecificComponent(
+  componentName: string,
+  rawAtributeName: string,
+  rawAttributeValue: string
+): string | null {
+  const attributeName = getNonShorthand(componentName, rawAtributeName) || rawAtributeName;
+  if (!attributeMap[attributeName]) {
+    return null;
+  }
+
+  // strip quote
+  const attributeValue = rawAttributeValue.slice(1, rawAttributeValue.length - 1);
+
+  return attributeMap[attributeName][attributeValue];
+}
--- src/rules/require-specific-component.ts
+++ src/rules/require-specific-component.ts
@@ -1,26 +1,26 @@
 import { AST_NODE_TYPES, TSESLint } from "@typescript-eslint/utils";
 import { isChakraElement } from "../lib/isChakraElement";
-import { getNonShorthand } from "../lib/getShorthand";
 import { ParserServices } from "@typescript-eslint/utils";
-import { ImportDeclaration } from "@typescript-eslint/types/dist/ast-spec";
-import { Symbol } from "typescript";
 import {
   JSXAttribute,
   JSXElement,
   JSXOpeningElement,
 } from "@typescript-eslint/utils/node_modules/@typescript-eslint/types/dist/ast-spec";
 import { RuleFix } from "@typescript-eslint/utils/dist/ts-eslint";
+import { getImportDeclarationOfJSX } from "../lib/getImportDeclaration";
+import { findSpecificComponent } from "../lib/findSpecificComponent";
 
 export const requireSpecificComponentRule: TSESLint.RuleModule<"requireSpecificComponent", []> = {
   meta: {
     type: "suggestion",
     docs: {
-      description: "Enforces the usage of shorthand Chakra component props.",
+      description: "Enforces the usage of specific Chakra component.",
       recommended: "error",
-      url: "https://github.com/Monchi/eslint-plugin-chakra-ui/blob/master/docs/rules/require-layout-component.md",
+      url: "https://github.com/Monchi/eslint-plugin-chakra-ui/blob/master/docs/rules/require-specific-component.md",
     },
     messages: {
-      requireSpecificComponent: "Component '{{invalidComponent}}' could be replaced by {{validComponent}}'.",
+      requireSpecificComponent:
+        "'{{invalidComponent}}' with attribute '{{attribute}}' could be replaced by '{{validComponent}}'.",
     },
     schema: [],
     fixable: "code",
@@ -44,57 +44,41 @@ export const requireSpecificComponentRule: TSESLint.RuleModule<"requireSpecificC
         }
 
         for (const attribute of node.attributes) {
-          if (attribute.type !== AST_NODE_TYPES.JSXAttribute) {
+          if (attribute.type !== AST_NODE_TYPES.JSXAttribute || attribute.value == null) {
             continue;
           }
 
-          if (attribute.value == null) {
+          const specificComponent = findSpecificComponent(
+            componentName,
+            sourceCode.getText(attribute.name),
+            sourceCode.getText(attribute.value)
+          );
+          if (specificComponent == null) {
             continue;
           }
 
-          const rawAttributeName = sourceCode.getText(attribute.name);
-          const attributeName = getNonShorthand(componentName, rawAttributeName) || rawAttributeName;
-          if (!attributeMap[attributeName]) {
-            continue;
-          }
+          report({
+            node: node,
+            messageId: "requireSpecificComponent",
+            data: {
+              invalidComponent: componentName,
+              validComponent: specificComponent,
+              attribute: sourceCode.getText(attribute),
+            },
+            fix(fixer) {
+              const renameStartTag = fixer.replaceText(node.name, specificComponent);
 
-          const rawAttributeValue = sourceCode.getText(attribute.value);
-          // strip quote
-          const attributeValue = rawAttributeValue.slice(1, rawAttributeValue.length - 1);
-          if (attributeMap[attributeName][attributeValue]) {
-            const validComponent = attributeMap[attributeName][attributeValue];
-            report({
-              node: node,
-              messageId: "requireSpecificComponent",
-              data: {
-                invalidComponent: componentName,
-                validComponent: validComponent,
-              },
-              fix(fixer) {
-                const renameStartTag = fixer.replaceText(node.name, validComponent);
-
-                return [
-                  renameStartTag,
-                  createFixToRenameEndTag(node, validComponent, fixer),
-                  createFixToRemoveAttribute(attribute, node, fixer),
-                  createFixToInsertImport(node, validComponent, parserServices, fixer),
-                ].filter((v) => !!v) as RuleFix[];
-              },
-            });
-          }
+              return [
+                renameStartTag,
+                createFixToRenameEndTag(node, specificComponent, fixer),
+                createFixToRemoveAttribute(attribute, node, fixer),
+                createFixToInsertImport(node, specificComponent, parserServices, fixer),
+              ].filter((v) => !!v) as RuleFix[];
+            },
+          });
         }
       },
     };
-  },
-};
-
-const attributeMap: Record<string, Record<string, string>> = {
-  display: {
-    flex: "Flex",
-    grid: "Grid",
-  },
-  as: {
-    img: "Image",
   },
 };
 
@@ -124,13 +108,13 @@ function createFixToInsertImport(
 ) {
   const importDecl = getImportDeclarationOfJSX(jsxNode, parserServices);
   if (!importDecl) {
-    throw new Error("No decl");
+    throw new Error("No ImportDeclaration found.");
   }
 
   const sameNameSpecifier = importDecl.specifiers.find(
     (sp) => sp.type === AST_NODE_TYPES.ImportSpecifier && sp.local.name === validComponent
   );
-  if (sameNameSpecifier) {
+  if (sameNameSpecifier != null) {
     // in case of already imported
     return null;
   }
@@ -143,29 +127,4 @@ function createFixToInsertImport(
   } else {
     return fixer.insertTextAfter(last, `, ${validComponent}`);
   }
-}
-
-function getImportDeclarationOfJSX(node: JSXOpeningElement, parserServices: ParserServices): ImportDeclaration | null {
-  const typeChecker = parserServices.program.getTypeChecker();
-  const tsNode = parserServices.esTreeNodeToTSNodeMap.get(node.name);
-  const symbol = typeChecker.getSymbolAtLocation(tsNode);
-  // string tag
-  if (symbol == null) {
-    return null;
-  }
-
-  return getImportDeclarationOfSymbol(symbol, parserServices);
-}
-
-function getImportDeclarationOfSymbol(
-  // eslint-disable-next-line @typescript-eslint/ban-types -- This Symbol is imported from "typescript"
-  symbol: Symbol,
-  parserServices: ParserServices
-): ImportDeclaration | null {
-  if (symbol.declarations == null || symbol.declarations.length < 1) {
-    return null;
-  }
-
-  const node = parserServices.tsNodeToESTreeNodeMap.get(symbol.declarations[0]).parent;
-  return node?.type === AST_NODE_TYPES.ImportDeclaration ? node : null;
 }
