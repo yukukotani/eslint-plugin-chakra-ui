c679c32cfe342bca97a7f035661d915c89c91e05
Yuku Kotani <yukukotani@gmail.com>
2022-02-27 21:38:16 +0900

    export require-specific-component rule correctly

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { propsOrderRule } from "./rules/props-order";
 import { propsShorthandRule } from "./rules/props-shorthand";
+import { requireSpecificComponentRule } from "./rules/require-specific-component";
 
 export = {
   rules: {
     "props-order": propsOrderRule,
     "props-shorthand": propsShorthandRule,
+    "require-specific-component": requireSpecificComponentRule,
   },
 };
